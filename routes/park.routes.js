fe856ed5a9f5759df21da89bdf03f16f0cb226a6
Javier Agudo <javieragudo3d@gmail.com>
2019-11-20 09:58:56 +0100

    Exercise 1,2,3,4

--- routes/park.routes.js
+++ routes/park.routes.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 // Aquí los endpoints
+router.get("/", (req, res, next) => {
+  res.render("parks/new-park");
+});
 
-module.exports = router
+module.exports = router;
