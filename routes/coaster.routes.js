89c0d2771b4192ba83fa18c293bad7a578e12293
Javier Agudo <javieragudo3d@gmail.com>
2019-11-20 11:26:42 +0100

    Add Exercise 5,6,7,8,9,10

--- routes/coaster.routes.js
+++ routes/coaster.routes.js
@@ -1,7 +1,39 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-// Aquí los endpoints
+const Park = require("../models/park.model");
+const Coaster = require("../models/coaster.model");
+
+router.get('/new', (req, res, next) => {
+	Park.find({},(error, parksFromDB) => {
+    console.log(parksFromDB)
+    
+		if (error) { 
+			next(error); 
+		} else { 
+			res.render('coasters/new-coaster', { parks: parksFromDB});
+		}
+	});
+});
+router.post("/new", (req, res, next) => {
+
+    const newCoaster = new Coaster({
+      name: req.body.name,
+      description: req.body.description,
+      inversions:	req.body.inversions,
+        length:		req.body.lenght,
+        active:		true,
+        park: req.body.park
+    });
+  
+    newCoaster.save(error => {
+      if (error) {
+        next(error);
+      } else {
+        res.redirect("/");
+      }
+    });
+  });
 
 
-module.exports = router
+module.exports = router;
--- routes/park.routes.js
+++ routes/park.routes.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const Park = require("../models/park.model");
 
 // Aquí los endpoints
-router.get("/", (req, res, next) => {
+router.get("/new", (req, res, next) => {
   res.render("parks/new-park");
 });
 
+router.post("/new", (req, res, next) => {
+
+  const newPark = new Park({
+    name: req.body.name,
+    description: req.body.description,
+    active: true
+  });
+
+  newPark.save(error => {
+    if (error) {
+      next(error);
+    } else {
+      res.redirect("/parks/new");
+    }
+  });
+});
 module.exports = router;
